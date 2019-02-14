be87634c5a19d8a1722bca1d6515835d89bc4785
luisatheodoro <theodoro.luisa@gmail.com>
2019-02-14 17:18:53 +0000

    User DB Schema

---
+++ tests/routes/user.test.js
@@ -0,0 +1,21 @@
+var expect = require('chai').expect;
+
+var User = require('../../models/User');
+
+describe('user', function() {
+
+  it('has a name, email, password', function(done) {
+
+    var newUser = new User({
+      name: 'my name',
+      email: 'blah@blah.com',
+      password: 'password123'
+    });
+
+    expect(newUser.name).to.equal('my name');
+    expect(newUser.email).to.equal('blah@blah.com');
+    expect(newUser.password).to.equal('password123');
+    done();
+  });
+});
+
