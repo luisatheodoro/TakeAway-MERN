a79cff9f1c117e976fe0568c54d876fe57658bbf
luisatheodoro <theodoro.luisa@gmail.com>
2019-02-14 17:45:27 +0000

    connecting to test database and cleaning database

--- tests/routes/user.test.js
+++ tests/routes/user.test.js
@@ -2,9 +2,20 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var should = chai.should();
 var server = require('../../server');
+var mongoose = require('mongoose');
 chai.use(chaiHttp);
+const db = require('../../config/keys').mongoTestURI;
 
 describe('Routes testing', function() {
+
+  beforeEach(function (done) {
+    mongoose.connect(db, function(){
+      mongoose.connection.db.dropDatabase(function(){
+        done();
+      })
+    })
+  });
+
   it('should GET /users', function(done) {
     chai.request(server)
         .get('/api/users')
@@ -17,7 +28,7 @@ describe('Routes testing', function() {
   it('should POST /users', function(done) {
     chai.request(server)
         .post('/api/users')
-        .send({'name': 'Lana', 'email': 'hello@gmail.com', 'password' : 'mybadpassword'})
+        .send({'name': 'Lan', 'email': 'hello@gmail.com', 'password' : 'mybadpassword'})
         .end(function(err, res){
           res.should.be.json;
           res.should.have.status(200);
